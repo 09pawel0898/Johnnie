b7c2bd8403ebbbe061fe5c3c2ba3f805324badad
09pawel0898 <pawel_2014@interia.eu>
2022-07-17 13:33:33 +0200

    [Engine|Core] EntryPoint added

---
+++ Engine/Source/Core/EntryPoint.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Application.hpp"
+
+using namespace Engine;
+
+extern std::shared_ptr<Core::Application> Core::CreateApplication(void);
+
+
+int main(void)
+{
+	Core::Application::s_Instance = Core::CreateApplication();
+	Core::Application::s_Instance->Run();
+	return 0;
+}
