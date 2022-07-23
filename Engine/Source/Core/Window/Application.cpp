763b96242399b5e5fc9f17f1fb693530ddf69f40
09pawel0898 <pawel_2014@interia.eu>
2022-07-23 12:42:38 +0200

    [Dependencies] add GL_NVX_gpu_memory_info and GL_ATI_meminfo extensions to glad

--- Engine/Source/Core/Window/Application.cpp
+++ Engine/Source/Core/Window/Application.cpp
@@ -58,7 +58,6 @@ namespace Engine::Core
 #if (_MSC_VER >= 1910)
         _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
-
         using TimePoint = std::chrono::high_resolution_clock;
 
         std::chrono::steady_clock::time_point tFrameStart, tLastUpdate;
